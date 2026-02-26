a899e462c73328b33dedb2b492d8f864e78013a1
Gabriel Almeida <dotdlenildaed4@gmail.com>
2026-02-26 12:52:55 +0000

    Added Smooth Operator and OperadoresEmInteiros

---
+++ OperadoresEmInteiros/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace OperadoresEmInteiros
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int i1 = 3 ;
+            Console.WriteLine(i1);
+
+            int i2 = 1 ;
+            Console.WriteLine(i2);
+
+            int i3 = -8 ;
+            Console.WriteLine(i3);
+
+            int x = 2 ;
+            Console.WriteLine(x);
+
+            x = i1 - i3 ;
+            Console.WriteLine(x);
+            x = i3 / i1 ;
+            Console.WriteLine(x);
+            x += i2 ;
+            Console.WriteLine(x);
+            x *= i1 ;
+            Console.WriteLine(x);
+            x %= i2 - 0b1001 ;
+            Console.WriteLine(x);
+            x = i1 << 2 ;
+            Console.WriteLine(x);
+            x <<= x ;
+            Console.WriteLine(x);
+            x &= 0x21AD ^ i1 ;
+            Console.WriteLine(x);
+            x = ~(i1|i3) ;
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insere número inteiro:");
+            string numero = Console.ReadLine();
+
+            sbyte n = Convert.ToSByte(numero);
+
+            Console.WriteLine("Decremento:");
+            Console.WriteLine(--n);
+
+            Console.WriteLine("Incremento:");
+            Console.WriteLine(++n);
+
+            Console.WriteLine("Com o número 127:");
+            int experiencia = 127;
+            sbyte n2 = Convert.ToSByte(experiencia);
+            Console.WriteLine("Decremento:");
+            Console.WriteLine(--n2);
+
+            Console.WriteLine("Incremento:");
+            Console.WriteLine(++n2);
+        }
+    }
+}
