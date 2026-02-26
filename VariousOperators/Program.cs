50a3167590afeb0dce4bb260aadfa17cecfbd7e5
Gabriel Almeida <dotdlenildaed4@gmail.com>
2026-02-26 14:14:36 +0000

    VariousOperators now working

--- VariousOperators/Program.cs
+++ VariousOperators/Program.cs
@@ -6,7 +6,22 @@ namespace VariousOperators
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            Console.WriteLine("Insere número não-inteiro negativo:");
+            string numero = Console.ReadLine();
+            byte n = byte.Parse(numero);
+
+            Console.WriteLine("Metade:");
+            Console.WriteLine(n / 2);
+
+            Console.WriteLine("Shift left por 3:");
+            Console.WriteLine(n << 3);
+
+            Console.WriteLine("n XOR 6:");
+            Console.WriteLine(n ^ 6);
+
+            Console.WriteLine("Maior que 10?:");
+            Console.WriteLine(n > 10);
+
         }
     }
 }
